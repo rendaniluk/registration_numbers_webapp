74f920324bd65cd7866bcaf48e1b0a190df445ee
rendaniluk <lukhwa11582924>
2017-08-22 10:18:27 +0200

    registration using routes

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express3-handlebars');
+const bodyParser = require('body-parser');
 
-
-app.get('/registration_number/:lic', function(req, res){
-  res.send('/', req.params.lic);
+app.get('/registration_number/:lic', function(req, res) {
+  var license = (req.params.lic).substr(0, 2).toUpperCase() + ' ' + (req.params.lic)
+            .substr(2).toLowerCase()
+  res.render('pages/index', {
+    licenceNum: license
+  });
 });
+
+
+//configuring handlebars
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+//using static
+app.use(express.static('public'));
+
+//using bodyParser
+app.use(bodyParser.urlencoded({
+    extended: false
+  }))
+  // parse application/json
+app.use(bodyParser.json());
 
 
 app.set('port', (process.env.PORT || 5000));
